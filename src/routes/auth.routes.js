7bebd718702be41d25165e53083fd2b90aa2b633
erick <erick.olviares.gonzalez@gmail.com>
2023-08-21 14:58:42 -0400

    se elimino la ruta logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,10 +7,7 @@ const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register)
 router.post("/login", validateSchema(loginSchema) ,login)
-router.post("/logout", logout)
 router.get("/profile", authRequired , profile)
-
-
 
 
 export default router;
